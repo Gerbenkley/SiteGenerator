54b31793736093b1faf98355982c306c658fe4ed
Gerbenkley <65512404+Gerbenkley@users.noreply.github.com>
2025-09-04 19:48:12 +0200

    inline and delimiter splitting added

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
+
 if __name__ == "__main__":
     main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
-from enum import Enum
 from htmlnode import LeafNode
+from enum import Enum
 
 
 class TextType(Enum):
